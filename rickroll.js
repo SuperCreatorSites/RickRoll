073eec45e9d4199682eaa9dc4dc6630c46dd591c
SuperCreatorSites <misasur420@gmail.com>
2024-09-21 21:57:34 +0300

    Add files via upload

---
+++ rickroll.js
@@ -0,0 +1,8 @@
+var otvets = ["Да", "да", "конечно", "Конечно", "Yes", "yes"];
+
+while (true) {
+    var rickroll = prompt("Ты лох?");
+    if (otvets.includes(rickroll)) {
+        break;
+    }
+}
