733dabfe102280831f52809cfed02b5be8127ca9
SuperCreatorSites <misasur420@gmail.com>
2024-09-21 22:32:25 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var otvets = ["Да", "да", "конечно", "Конечно", "Yes", "yes"];
-
-while (true) {
-    var rickroll = prompt("Ты лох?");
-    if (otvets.includes(rickroll)) {
-        break;
-    }
-}
